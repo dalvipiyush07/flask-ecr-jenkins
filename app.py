05c813504edb49d6d1ba955808acdebb79d3c703
Piyush Pravinkumar Dalvi <piyushdalvi65@gmail.com>
2026-03-07 18:21:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello from Project 4! Docker + Jenkins + ECR + Lambda'
+    return 'Hello from Project 4! Docker + Jenkins + ECR + Lambda + final done by P.D.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
